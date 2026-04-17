f84edfb126f8d1bb3d5d55ce7e642c8b934fb5c7
CigraRamadhan <cigraramadhanrp@gmail.com>
2026-04-17 15:34:58 +0700

    Commit Pertama

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TestController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -9,3 +10,13 @@ Route::get('/', function () {
 Route::get('/test', function () {
     return view('testing');
 });
+
+Route::get('/user/{id}', function($id) {
+    return "Profil User dengan ID:".$id;
+});
+
+Route::get('/produk/{slug?}', function($slug='semua-produk') {
+    return "Menampilkan ".$slug;
+});
+
+Route::get('/mahasiswa', [TestController::class, 'index']);
