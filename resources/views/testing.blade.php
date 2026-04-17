594f6104d1921c827498691c38e9d0c0d88ffe58
CigraRamadhan <cigraramadhanrp@gmail.com>
2026-04-17 15:34:52 +0700

    Bebas

---
+++ resources/views/testing.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <h2>Selamat Datang!</h2>
+
+        <li>
+            <ul>Bagian A</ul>
+            <ul>Bagian B</ul>
+            <ul>Bagian C</ul>
+        </li>
+    </div>
+</body>
+</html>
